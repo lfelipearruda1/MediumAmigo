1e4560a987bdd29bf5bb63ba68dcbbd12e97db4a
lfelipearruda1 <lfelipearrudacc@gmail.com>
2024-12-05 17:41:10 -0300

    Commit Create User-Id e deleted_at

---
+++ src/database/migrations/20241205203905-create-userid.js
@@ -0,0 +1,49 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+  
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      user_id: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				references: {
+					model: "users",
+					key: "id",
+				},
+			},
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      deleted_at: {
+				type: Sequelize.DATE,
+				allowNull: true,
+			},
+    });
+
+  },
+
+  async down (queryInterface, Sequelize) {
+   return queryInterface.dropTable('users');
+  }
+};
