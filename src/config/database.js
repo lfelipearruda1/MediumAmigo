23bcc467f621adc70c625af4ce96686aceeb6db8
lfelipearruda1 <lfelipearrudacc@gmail.com>
2024-12-05 16:35:14 -0300

    commit

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,14 @@
 const { timeStamp } = require("console");
 
-module.exports ={
-    dialect: 'postgres',
-    host: 'localhost',
-    username: 'docker',
-    passowrd: 'docker',
-    databese: 'sqlnode',
-    define:{
-        timestamps: true,
-        underscored: true,
-    },
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'docker', // Usuário do banco de dados
+  password: 'docker', // Senha do usuário
+  database: 'sqlnode', // Nome do banco
+  port: 5432, // Porta do PostgreSQL
+  define: {
+    timestamps: true, // Cria os campos createdAt e updatedAt automaticamente
+    underscored: true, // Usa snake_case para nomes de tabelas e colunas
+  },
 };
-
-//created_at, update_at
---
+++ src/database/migrations/20241205012405-create-users.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+  
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
