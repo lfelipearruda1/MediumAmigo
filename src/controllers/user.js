0a5c6453034a1bb5cd2d8310b39b1145d6000c57
lfelipearruda1 <lfelipearrudacc@gmail.com>
2024-12-20 16:45:50 -0300

    CRUD de usuario

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -4,11 +4,92 @@ class UserController {
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-
       return res.json(novoUser);
     } catch (e) {
       return res.status(500).json({
-        errors: e.error.map(err => err.message)
+        errors: e.errors.map(err => err.message),
+      });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      return res.status(500).json({
+        errors: ['Erro ao buscar usuários'],
+      });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não encontrado'],
+        });
+      }
+      return res.json(user);
+    } catch (e) {
+      return res.status(500).json({
+        errors: ['Erro ao buscar usuário'],
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não enviado'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe'],
+        });
+      }
+
+      const novoDados = await user.update(req.body);
+
+      return res.json(novoDados);
+    } catch (e) {
+      return res.status(500).json({
+        errors: ['Erro ao atualizar usuário'],
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não enviado'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe'],
+        });
+      }
+
+      await user.destroy();
+
+      return res.json({
+        success: true,
+        message: 'Usuário deletado com sucesso',
+      });
+    } catch (e) {
+      return res.status(500).json({
+        errors: ['Erro ao deletar usuário'],
       });
     }
   }
--- src/routes/user.js
+++ src/routes/user.js
@@ -4,5 +4,9 @@ import userController from '../controllers/user';
 const router = new Router();
 
 router.post('/', userController.store);
+router.get('/', userController.index);
+router.get('/:id', userController.show);
+router.get('/:id', userController.update);
+router.get('/:id', userController.delete);
 
 export default router;
