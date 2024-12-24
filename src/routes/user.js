347ab8044325a2e63621cda0a75ea70bfa02faf5
lfelipearruda1 <lfelipearrudacc@gmail.com>
2024-12-24 15:07:20 -0300

    Middleware de verificação do JWT

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,12 @@
-import { Router } from "express";
+import { Router } from 'express';
 import userController from '../controllers/user';
+
+import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
 router.post('/', userController.store);
-router.get('/', userController.index);
+router.get('/', loginRequired ,userController.index);
 router.get('/:id', userController.show);
 router.get('/:id', userController.update);
 router.get('/:id', userController.delete);
