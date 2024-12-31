b24998d346c326f280619fd37cc55a71e0e8df83
lfelipearruda1 <lfelipearrudacc@gmail.com>
2024-12-31 19:16:35 -0300

    Tratando os arquivos enviados

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import user from './controllers/user.js'; // Atualize para a sintaxe de importação
+import user from './controllers/UserController.js';
 
 const routes = Router();
 
 routes.get('/users', user.index);
 routes.post('/users', user.store);
 
-export default routes;  // Exportando com 'export default'
+export default routes;
--- src/server.js
+++ src/server.js
@@ -4,11 +4,16 @@ import dotenv from 'dotenv';
 import './database/index.js';
 import routes from './routes.js';
 
+dotenv.config();
+
 const app = express();
 
 app.use(express.json());
+
 app.use(routes);
 
-app.listen(5432, () => {
-  console.log('Server is running on port 3333');
+const PORT = process.env.PORT || 5432;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
