e407700e4b4371ffb91077317b0c5bb03fc4d7b7
lfelipearruda1 <lfelipearrudacc@gmail.com>
2025-01-07 09:52:48 -0300

    Commit

--- API/src/config/database.js
+++ API/src/config/database.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
+
 module.exports = {
-  username: process.env.DB_USER || 'root', 
-  password: process.env.DB_PASSWORD || null,
-  database: process.env.DB_NAME || 'mydatabase',
-  host: process.env.DB_HOST || 'localhost',
-  dialect: 'postgres',
+  development: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',          
+  },
 };
--- api/src/database/migrations/20250106191233-create-usuarios-table.js
+++ api/src/database/migrations/20250106191233-create-usuarios-table.js
@@ -2,21 +2,49 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('mediumapi', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      userImg: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      bgImg: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('NOW'),
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('NOW'),
+      },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('mediumapi');
+  },
 };
